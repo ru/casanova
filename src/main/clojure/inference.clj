a0ff7bbca7190a22700d289c1ef4a4686965e25c
Árni Hermann Reynisson <arni@gagnavarslan.is>
2010-03-16 21:46:08 +0000

    Updated to use partition concept.

--- src/main/clojure/inference.clj
+++ src/main/clojure/inference.clj
@@ -14,46 +14,25 @@
   (eval (read-string (read-gdl filename))))
 
 
-(defn filter-when-first
-  "Returns lists from a collection when the first item in the list matches a given paramter."
-  [first-item coll]
-  (filter #(= first-item (first %)) coll))
-
-
-(def special-implications? #{'next 'legal 'goal 'terminal})
-
-
-(defn build-model
-  "Builds a model of a game description."
+(defn partition-gdl
+  "Partitions game description into roles, initial-state, user-defined views,
+  next-states, legal-moves, goals and terminals."
   [filename]
   (let [gdl (eval-gdl filename)
+        filter-when-first (fn [first-item coll] (filter #(= first-item (first %)) coll))
         roles (filter-when-first 'role gdl)
         initial-state (filter-when-first 'init gdl)
+        special-implications? #{'next 'legal 'goal 'terminal}
         implications (filter-when-first '<= gdl)
         implication-name (fn [i] (let [r (rest i)] (if (coll? (first r)) (first (first r)) (first r))))
         special-implications (filter #(special-implications? (implication-name %)) implications)
         user-defined-implications (filter #(not (special-implications? (implication-name %))) implications)
-        next (filter #(= 'next (implication-name %)) special-implications)
+        next-states (filter #(= 'next (implication-name %)) special-implications)
         legal-moves (filter #(= 'legal (implication-name %)) special-implications)
         goals (filter #(= 'goal (implication-name %)) special-implications)
         terminals (filter #(= 'terminal (implication-name %)) special-implications)]
-    (println roles)
-    (println "********")
-    (println initial-state)
-    (println "********")
-    (println implications)
-    (println "********")
-    (println special-implications)
-    (println "********")
-    (println user-defined-implications)
-    (println "********")
-    (println next)
-    (println "********")
-    (println legal-moves)
-    (println "********")
-    (println goals)
-    (println "********")
-    (println terminals)))
+    {:roles roles, :initial-state initial-state, :user-defined user-defined-implications,
+     :next-states next-states, :legal-moves legal-moves, :goals goals, :terminals terminals}))
 
 
 ;;; alias for tic-tac-toe game description, useful on the repl
