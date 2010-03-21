fcb96e0956fb8f25919d4802a3b083bbf199e3ed
Haukur Palmason <haukurpalmason@flatey.local>
2010-03-21 14:21:08 +0000

    Haukur

--- source/is/ru/ggp/singleagent/lists/ClosedList.java
+++ source/is/ru/ggp/singleagent/lists/ClosedList.java
@@ -1,5 +1,26 @@
 package is.ru.ggp.singleagent.lists;
+import java.util.HashMap;
 
-public class ClosedList {
+public class ClosedList implements IClosedList{
+	
+	//private HashMap<K, V>
+
+	@Override
+	public void addToList(String stateId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFromList(String stateId) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
