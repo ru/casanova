1e17c5b1e92cb58a7041fb1c86691d38f39f857c
Haukur Palmason <haukurpalmason@flatey.local>
2010-03-21 15:25:04 +0000

    Ok slatti af breytingum

--- source/is/ru/ggp/singleagent/lists/ClosedList.java
+++ source/is/ru/ggp/singleagent/lists/ClosedList.java
@@ -1,26 +1,39 @@
 package is.ru.ggp.singleagent.lists;
 import java.util.HashMap;
 
+import org.eclipse.palamedes.gdl.core.model.IGameNode;
+
 public class ClosedList implements IClosedList{
 	
-	//private HashMap<K, V>
+	private HashMap <String, IGameNode> hashmap;
+	
+	public ClosedList(){
+		hashmap = new HashMap <String, IGameNode>();
+	}
 
+	private String createStateId (final IGameNode node){
+		return "String";
+		//TODO createStateID
+	}
+	
 	@Override
-	public void addToList(String stateId) {
-		// TODO Auto-generated method stub
-		
+	public void addToList(IGameNode node) {
+		hashmap.put(createStateId(node), node);
 	}
 
 	@Override
 	public void clearList() {
-		// TODO Auto-generated method stub
-		
+		hashmap.clear();	
 	}
 
 	@Override
 	public void removeFromList(String stateId) {
-		// TODO Auto-generated method stub
-		
+		hashmap.remove(stateId);
+	}
+
+	@Override
+	public boolean contains(IGameNode node) {
+		return hashmap.containsValue(node);
 	}
 
 }
--- source/is/ru/ggp/singleagent/lists/IClosedList.java
+++ source/is/ru/ggp/singleagent/lists/IClosedList.java
@@ -1,10 +1,13 @@
 package is.ru.ggp.singleagent.lists;
 
+import org.eclipse.palamedes.gdl.core.model.IGameNode;
+
 /**
  * Interface for the closed list which is used in the A* algorithm.
  */
 public interface IClosedList {
 	void clearList();
-	void addToList(String stateId);
+	void addToList(IGameNode node);
 	void removeFromList(String stateId);
+	boolean contains(IGameNode node);
 }
